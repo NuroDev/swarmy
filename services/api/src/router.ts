672d55eaada4655893d70cf161740888017b5aee
nurodev <ben@nuro.dev>
2024-04-07 15:24:05 +0100

    Added logger middleware to `api` service

--- services/api/src/router.ts
+++ services/api/src/router.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 import { healthRoute } from "~/routes/health.route";
 import { indexRoute } from "~/routes/index.route";
 
-export const app = new Hono().route("/", indexRoute).route("/", healthRoute);
+export const app = new Hono()
+  .use(logger())
+  .route("/", indexRoute)
+  .route("/", healthRoute);
 
 export type App = typeof app;
