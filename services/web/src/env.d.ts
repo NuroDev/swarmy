492b41aabad8f16b5579831563b3be56a610540a
nurodev <ben@nuro.dev>
2024-04-07 15:17:59 +0100

    Added types env vars to `web` service

--- services/web/src/env.d.ts
+++ services/web/src/env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="astro/client" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly API_URL: string;
+    readonly HOST: string;
+    readonly HOSTNAME: string;
+    readonly NODE_ENV: "development" | "production";
+    readonly PORT: string;
+  }
+}
+
+declare module "bun" {
+  interface Env extends NodeJS.ProcessEnv {}
+}
+
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
